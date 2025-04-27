82a93f5c91dfe70b62da54f7f581657a96d7b3f1
Pradeep <pradeep14.3.2005@gmail.com>
2025-04-27 14:09:54 +0530

    student

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,6 @@
 package org.example;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        ApplicationContext context =new ClassPathXmlApplicationContext("Spring.xml");
         System.out.println( "Hello World!" );
+        Student std= context.getBean(Student.class);
+
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Student {
+    public Student(){
+        System.out.print("Created");
+    }
+
+}
